8c0cf33649a97c716241f873ef4dd94da1121b5b
Joseph Dean <josephrdean@gmail.com>
2024-02-20 13:35:06 -0700

    Simple thing before I futz with compiling the desciptor file.

--- src/main/java/ProtobufMultiFileParser/ProtobufMultiFileParser.java
+++ src/main/java/ProtobufMultiFileParser/ProtobufMultiFileParser.java
@@ -1,1 +1,35 @@
 package ProtobufMultiFileParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+import com.google.protobuf.DescriptorProtos;
+
+/**
+ *
+ * @param baseMessageType The name of the root message type.
+ * @param descriptorFile The protoc generated Descriptor File that holds definitions of the message
+ *                       classes we're parsing.
+ * @param baseFile The root of the file hierarchy to parse.
+ */
+public class ProtobufMultiFileParser {
+
+    // Parses a set
+    public static Message Parse(String baseMessageType, File descriptorFile, File baseFile) throws InvalidProtocolBufferException {
+
+        byte[] descriptorBytes;
+        try {
+            descriptorBytes = Files.readAllBytes(descriptorFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        DescriptorProtos.FileDescriptorProto descriptorProto = DescriptorProtos.FileDescriptorProto.parseFrom(descriptorBytes);
+
+        // Just make it compile for now. We'll need to find the matching message, then parse the contents of
+        // baseFile into it.
+        return descriptorProto.getMessageTypeList().getFirst().newBuilderForType().build();
+    }
+}
