9f83309eadf958cd98c153e5cc0e93bf9d186e2d
Joseph Dean <josephrdean@gmail.com>
2024-02-23 15:14:53 -0700

    Original working implementation that reads one root file. Now let's recurse.

--- src/main/java/ProtobufMultiFileParser/ProtobufMultiFileParser.java
+++ src/main/java/ProtobufMultiFileParser/ProtobufMultiFileParser.java
@@ -1,17 +1,10 @@
 package ProtobufMultiFileParser;
 
-import com.google.protobuf.DescriptorProtos;
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.Message;
+import com.google.protobuf.*;
 import com.google.protobuf.util.JsonFormat;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
+import java.net.URL;
 
 public class ProtobufMultiFileParser {
 
@@ -20,45 +13,23 @@ public class ProtobufMultiFileParser {
      * @param baseMessageType The name of the root message type.
      * @param descriptorFile The protoc generated Descriptor File that holds definitions of the message
      *                       classes we're parsing.
-     * @param baseFile The root of the file hierarchy to parse.
+     * @param contentRoot The URL of the root object to parse.
      */
-    public static Message Parse(String baseMessageType, InputStream descriptorFile, String jsonContent) throws IOException {
+    public static Message Parse(String baseMessageType, URL descriptorFile, URL contentRoot) throws IOException, Descriptors.DescriptorValidationException {
+        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorFile.openStream());
 
-        byte[] descriptorBytes;
-        try {
-            descriptorBytes = descriptorFile.readAllBytes();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder();
+        for (var e : descriptorSet.getFileList()) {
+            typeRegistryBuilder.add(
+                    Descriptors.FileDescriptor.buildFrom(e, new Descriptors.FileDescriptor[]{}).getMessageTypes());
         }
+        TypeRegistry typeRegistry = typeRegistryBuilder.build();
 
+        Descriptors.Descriptor descriptor = typeRegistry.find(baseMessageType);
+        DynamicMessage.Builder messageBuilder = DynamicMessage.newBuilder(descriptor);
 
-        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
-        List<DescriptorProtos.DescriptorProto> descriptorProtos = descriptorSet.getFileList().stream().flatMap(e -> e.getMessageTypeList().stream()).toList();
-        List<DescriptorProtos.DescriptorProto> candidates = descriptorProtos.stream().filter(e -> e.getName().equals(baseMessageType)).toList();
-
-        if (candidates.stream().findAny().isEmpty()) {
-            throw new IllegalArgumentException("Message type not found in descriptor set. Candidates: "
-                    + String.join(", ", descriptorProtos.stream().map(DescriptorProtos.DescriptorProto::getName).toList()));
-        } else if (candidates.size() > 1) {
-            throw new IllegalArgumentException("Found multiple candidate matches.");
-        }
-        DescriptorProtos.DescriptorProto descriptorProto = candidates.getFirst();
-
-
-//        Message.Builder builder = descriptorProto.newBuilderForType();
-//        com.google.protobuf.util.JsonFormat;
-//        JsonFormat.parser().ignoringUnknownFields().merge(json, structBuilder);
-//        return structBuilder.build();
-//
-        Descriptors.Descriptor descriptor = descriptorProto.getDescriptorForType();
-        DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor);
-
-
-//        dynamicBuilder.getDefaultInstanceForType().buil
-
-
-        com.google.protobuf.util.JsonFormat.parser().merge(jsonContent, dynamicBuilder);
-        Message message = dynamicBuilder.build();
-        return message;
+        String content = new String(contentRoot.openStream().readAllBytes());
+        JsonFormat.parser().merge(content, messageBuilder);
+        return messageBuilder.build();
     }
 }
--- src/test/java/TestProtobufMultiFileParser.java
+++ src/test/java/TestProtobufMultiFileParser.java
@@ -1,81 +1,67 @@
-import com.google.common.io.Resources;
+import ProtobufMultiFileParser.ProtobufMultiFileParser;
 import com.google.protobuf.*;
 import com.google.protobuf.util.JsonFormat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import org.junit.jupiter.api.Assertions;
 
 public class TestProtobufMultiFileParser {
 
-//    @Test
-//    void testParseFile() throws IOException {
-//        URL url = Resources.getResource("base_message.json");
-//        String jsonFile = Resources.toString(url, StandardCharsets.UTF_8);
-//
-//        InputStream descriptor = getClass().getClassLoader().getResourceAsStream("main.dsc");
-//        assert descriptor != null;
-//        ProtobufMultiFileParser.ProtobufMultiFileParser.Parse("base_message", descriptor, jsonFile);
-//    }
 
+    /**
+     * Dynamic parsing with protobuf took me a little effort to get right, so I'm leaving this test case.
+     * 1.) Output the descriptor file main.dsc during the build process
+     * 2.) Parse this as a FileDescriptorSet
+     * 3.) Create a FileDescriptor for each File in the DescriptorSet. This is the step that threw me off. You already
+     * get a list of FileDescriptorProtos that contain DescriptorProtos for each message. These worked for parsing
+     * protobuf binary, but failed for protobuf Json.
+     * 4.) Create a TypeRegistry containing all of these. It gives nice lookup methods for types.
+     */
     @Test
-    void testMakeTypeRegistry() throws IOException, Descriptors.DescriptorValidationException {
-        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
-        assert descriptorFile != null;
-        byte[] descriptorBytes = descriptorFile.readAllBytes();
+    void testProtobufDynamicParsing() throws IOException, Descriptors.DescriptorValidationException {
+        DescriptorProtos.FileDescriptorSet descriptorSet =
+                DescriptorProtos.FileDescriptorSet.parseFrom(
+                        getClass().getClassLoader().getResourceAsStream("main.dsc"));
 
-        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
-
-        Descriptors.FileDescriptor fileDescriptor = Descriptors.FileDescriptor.buildFrom(descriptorSet.getFileList().getFirst(), new Descriptors.FileDescriptor[]{});
-
-        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder().add(fileDescriptor.getMessageTypes());
+        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder();
+        for (var e : descriptorSet.getFileList()) {
+            typeRegistryBuilder.add(
+                    Descriptors.FileDescriptor.buildFrom(e, new Descriptors.FileDescriptor[]{}).getMessageTypes());
+        }
         TypeRegistry typeRegistry = typeRegistryBuilder.build();
 
-        Descriptors.Descriptor descriptor1 = typeRegistry.find("BaseMessage");
+        Descriptors.Descriptor descriptor = typeRegistry.find("BaseMessage");
 
         ExampleMessage.BaseMessage baseMessage = ExampleMessage.BaseMessage.newBuilder()
                 .setBase(7).build();
-        DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor1);
-        dynamicBuilder.mergeFrom(baseMessage.toByteArray());
-        DynamicMessage dynamicMessage = dynamicBuilder.build();
-        Object obj = dynamicMessage.getField(descriptor1.findFieldByName("base"));
-        Assertions.assertEquals(7, (Integer)obj);
 
-        System.out.println("hi");
+        // Test binary parsing
+        {
+            DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor);
+            dynamicBuilder.mergeFrom(baseMessage.toByteArray());
+            DynamicMessage dynamicMessage = dynamicBuilder.build();
+            Object obj = dynamicMessage.getField(descriptor.findFieldByName("base"));
+            Assertions.assertEquals(7, (Integer) obj);
+        }
+
+        // Test json parsing
+        {
+            DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor);
+            String json = JsonFormat.printer().print(baseMessage);
+            JsonFormat.parser().merge(json, dynamicBuilder);
+            DynamicMessage dynamicMessage = dynamicBuilder.build();
+            Object obj = dynamicMessage.getField(descriptor.findFieldByName("base"));
+            Assertions.assertEquals(7, (Integer) obj);
+        }
     }
 
     @Test
-    void testJson() throws IOException, Descriptors.DescriptorValidationException {
-        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
-        assert descriptorFile != null;
-        byte[] descriptorBytes = descriptorFile.readAllBytes();
-
-        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
-
-        Descriptors.FileDescriptor fileDescriptor = Descriptors.FileDescriptor.buildFrom(descriptorSet.getFileList().getFirst(), new Descriptors.FileDescriptor[]{});
-
-        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder().add(fileDescriptor.getMessageTypes());
-        TypeRegistry typeRegistry = typeRegistryBuilder.build();
-
-        Descriptors.Descriptor descriptor1 = typeRegistry.find("BaseMessage");
-
-        ExampleMessage.BaseMessage baseMessage = ExampleMessage.BaseMessage.newBuilder()
-                .setBase(7).build();
-        String json = JsonFormat.printer().print(baseMessage);
-
-        DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor1);
-
-        JsonFormat.parser().merge(json, dynamicBuilder);
-
-        DynamicMessage dynamicMessage = dynamicBuilder.build();
-        Object obj = dynamicMessage.getField(descriptor1.findFieldByName("base"));
-        Assertions.assertEquals(7, (Integer)obj);
-
-        System.out.println("hi");
+    void testBasicTestCase() throws Descriptors.DescriptorValidationException, IOException {
+        URL descriptor = getClass().getClassLoader().getResource("main.dsc");
+        URL contentRoot = getClass().getClassLoader().getResource("base_message.json");
+        Message message = ProtobufMultiFileParser.Parse("BaseMessage", descriptor, contentRoot);
     }
 }
