9c73f28d7d42f2b39d20cc0792fbd37d79809b7a
Joseph Dean <josephrdean@gmail.com>
2024-02-23 10:49:52 -0700

    The magic is making a FileDescriptor. Slowly switching to that.

--- src/test/java/TestProtobufMultiFileParser.java
+++ src/test/java/TestProtobufMultiFileParser.java
@@ -3,11 +3,13 @@ import com.google.protobuf.*;
 import com.google.protobuf.util.JsonFormat;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 
 public class TestProtobufMultiFileParser {
 
@@ -20,6 +22,45 @@ public class TestProtobufMultiFileParser {
         assert descriptor != null;
         ProtobufMultiFileParser.ProtobufMultiFileParser.Parse("base_message", descriptor, jsonFile);
     }
+
+    @Test
+    void testMakeTypeRegistry() throws IOException, Descriptors.DescriptorValidationException {
+        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
+        assert descriptorFile != null;
+        byte[] descriptorBytes = descriptorFile.readAllBytes();
+
+        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
+
+        Descriptors.FileDescriptor fileDescriptor = Descriptors.FileDescriptor.buildFrom(descriptorSet.getFileList().getFirst(), new Descriptors.FileDescriptor[]{});
+
+        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder().add(fileDescriptor.getMessageTypes());
+        TypeRegistry typeRegistry = typeRegistryBuilder.build();
+
+        Descriptors.Descriptor descriptor1 = typeRegistry.find("BaseMessage");
+
+        ExampleMessage.BaseMessage baseMessage = ExampleMessage.BaseMessage.newBuilder()
+                .setBase(7).build();
+        DynamicMessage.Builder dynamicBuilder = DynamicMessage.newBuilder(descriptor1);
+        dynamicBuilder.mergeFrom(baseMessage.toByteArray());
+        DynamicMessage dynamicMessage = dynamicBuilder.build();
+        Object obj = dynamicMessage.getField(descriptor1.findFieldByName("base"));
+
+        System.out.println("hi");
+    }
+
+    // Create a local message instance, serialize it, parse it into a DynamicMessage, read a field back.
+    @Test
+    void testCompileTypeProtobuf() throws InvalidProtocolBufferException {
+        ExampleMessage.BaseMessage baseMessage = ExampleMessage.BaseMessage.newBuilder().setBase(7).build();
+        DynamicMessage.Builder dynamicMessage = DynamicMessage.newBuilder(ExampleMessage.BaseMessage.getDescriptor());
+        dynamicMessage.mergeFrom(baseMessage.toByteArray());
+
+        Object field = dynamicMessage.getField(ExampleMessage.BaseMessage.getDescriptor().findFieldByName("base"));
+        Integer intField = (Integer)field;
+        Assertions.assertEquals(7, intField);
+    }
+
+
 
     @Test
     void messWithProtobufDynamicMessage() {
@@ -70,15 +111,21 @@ public class TestProtobufMultiFileParser {
         DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
         List<DescriptorProtos.DescriptorProto> descriptorProtos = descriptorSet.getFileList().stream().flatMap(e -> e.getMessageTypeList().stream()).toList();
         List<DescriptorProtos.DescriptorProto> candidates = descriptorProtos.stream().filter(e -> e.getName().equals("BaseMessage")).toList();
+        DescriptorProtos.DescriptorProto descriptorProto = candidates.getFirst();
 
-        DynamicMessage.Builder dynBuilder = DynamicMessage.newBuilder(candidates.getFirst());
+        DynamicMessage.Builder dynBuilder = DynamicMessage.newBuilder(descriptorProto);
 
         byte[] exampleBytes = ExampleMessage.BaseMessage.newBuilder()
                 .setBase(7).build().toByteArray();
-
         dynBuilder.mergeFrom(exampleBytes);
+        Descriptors.FieldDescriptor fieldDescriptor = dynBuilder.getDescriptorForType().findFieldByName("base");
+        Object whatIsThis = dynBuilder.getField(dynBuilder.getDescriptorForType().findFieldByName("base"));
+        Integer myInt = (Integer)whatIsThis;
+
+        Descriptors.FieldDescriptor baseField = ExampleMessage.BaseMessage.getDescriptor().findFieldByName("base");
 
         Message dynMessage = dynBuilder.build();
+        org.junit.jupiter.api.Assertions.assertEquals(7, dynMessage.getField(baseField));
 
         System.out.println("foo");
     }
