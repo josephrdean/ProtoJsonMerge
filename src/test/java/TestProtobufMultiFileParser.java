22113f3320e3e49526ebed31655e64e458f8fbfa
Joseph Dean <josephrdean@gmail.com>
2024-02-23 09:27:50 -0700

    A quick checkpoint... it seems DynamicMessage with a Descriptor works for binary but not for json. Inspecting the descriptor object, the descriptor obtained from the DescriptorSet is slightly different than from the static type. But it does include the fields it should. So wtf.
    I'm going to take a step back and focus on the protobuf semantics with DynamicMessage, and test cases with binary/json.

--- src/test/java/TestProtobufMultiFileParser.java
+++ src/test/java/TestProtobufMultiFileParser.java
@@ -1,12 +1,13 @@
 import com.google.common.io.Resources;
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.*;
+import com.google.protobuf.util.JsonFormat;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class TestProtobufMultiFileParser {
 
@@ -16,6 +17,7 @@ public class TestProtobufMultiFileParser {
         String jsonFile = Resources.toString(url, StandardCharsets.UTF_8);
 
         InputStream descriptor = getClass().getClassLoader().getResourceAsStream("main.dsc");
+        assert descriptor != null;
         ProtobufMultiFileParser.ProtobufMultiFileParser.Parse("base_message", descriptor, jsonFile);
     }
 
@@ -32,4 +34,83 @@ public class TestProtobufMultiFileParser {
 //        DynamicMessage nested_message2 = DynamicMessage.newBuilder(descriptor).getDefaultInstanceForType();
         System.out.println("foo");
     }
+
+    @Test
+    void parseFromJsonFails() throws IOException {
+        URL url = Resources.getResource("base_message.json");
+        String jsonFile = Resources.toString(url, StandardCharsets.UTF_8);
+
+        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
+        assert descriptorFile != null;
+        byte[] descriptorBytes = descriptorFile.readAllBytes();
+
+        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
+        List<DescriptorProtos.DescriptorProto> descriptorProtos = descriptorSet.getFileList().stream().flatMap(e -> e.getMessageTypeList().stream()).toList();
+        List<DescriptorProtos.DescriptorProto> candidates = descriptorProtos.stream().filter(e -> e.getName().equals("BaseMessage")).toList();
+
+        // Strangely the constructor for DynamicMessage.Builder accepts both DescriptorProto and Descriptor.
+        // DescriptorProto seems more correct upon inspecting the runtime object... it actually contains the fields...
+        DescriptorProtos.DescriptorProto descriptorProto = candidates.getFirst();
+        DynamicMessage.Builder dynBuilder = DynamicMessage.newBuilder(descriptorProto);
+
+        // This is the version for the underlying descriptor, which seems less likely to work.
+//        Descriptors.Descriptor dynamicDescriptor = descriptorProto.getDescriptorForType();
+//        DynamicMessage.Builder dynBuilder = DynamicMessage.newBuilder(dynamicDescriptor);
+
+        JsonFormat.parser().merge(jsonFile, dynBuilder);
+        System.out.println("foo");
+    }
+
+    @Test
+    void parseFromBinary() throws IOException {
+        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
+        byte[] descriptorBytes;
+        descriptorBytes = descriptorFile.readAllBytes();
+
+        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
+        List<DescriptorProtos.DescriptorProto> descriptorProtos = descriptorSet.getFileList().stream().flatMap(e -> e.getMessageTypeList().stream()).toList();
+        List<DescriptorProtos.DescriptorProto> candidates = descriptorProtos.stream().filter(e -> e.getName().equals("BaseMessage")).toList();
+
+        DynamicMessage.Builder dynBuilder = DynamicMessage.newBuilder(candidates.getFirst());
+
+        byte[] exampleBytes = ExampleMessage.BaseMessage.newBuilder()
+                .setBase(7).build().toByteArray();
+
+        dynBuilder.mergeFrom(exampleBytes);
+
+        Message dynMessage = dynBuilder.build();
+
+        System.out.println("foo");
+    }
+
+    @Test
+    void parseWithStaticDescriptorWorks() throws IOException {
+        URL url = Resources.getResource("base_message.json");
+        String jsonFile = Resources.toString(url, StandardCharsets.UTF_8);
+
+        Descriptors.Descriptor staticDescriptor = ExampleMessage.BaseMessage.getDescriptor();
+        DynamicMessage.Builder builder = DynamicMessage.newBuilder(staticDescriptor);
+
+        JsonFormat.parser().merge(jsonFile, builder);
+        System.out.println("foo");
+    }
+
+//    @Test
+//    void testTypeRegistryApi() throws IOException {
+//        TypeRegistry typeRegistry = TypeRegistry.newBuilder().build();
+//
+//        InputStream descriptorFile = getClass().getClassLoader().getResourceAsStream("main.dsc");
+//        byte[] descriptorBytes;
+//        descriptorBytes = descriptorFile.readAllBytes();
+//        DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(descriptorBytes);
+//
+//        descriptorSet.getFileList().stream().flatMap(
+//
+//
+//        TypeRegistry.Builder typeRegistryBuilder = TypeRegistry.newBuilder();
+//        descriptorSet.getFileList().getFirst().getMessageTypeList().getFirst();
+//        typeRegistryBuilder.add
+//        descriptorSet.getFileList().stream().forEach(e -> typeRegistryBuilder.add(e.getMessageTypeList()));
+//        typeRegistryBuilder.add()
+//    }
 }
