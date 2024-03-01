a4dbbd250f878579bcd9c13b46b4ccbef8777b82
Joseph Dean <josephrdean@gmail.com>
2024-03-01 13:19:50 -0700

    Theoretically this is a gradle plugin now. How will I test this? I want to install it to my local .m2 and use it in another project...

---
+++ src/main/java/ProtobufMultiFileParser/MultiJsonMergerExtension.java
@@ -0,0 +1,9 @@
+package ProtobufMultiFileParser;
+
+public class MultiJsonMergerExtension {
+    public String descriptorFilePath;
+    public String contentRoot;
+    public String messageType;
+    public String outputJson;
+    public String outputPb;
+}
